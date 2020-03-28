b18cd1cf3fd6b534eae108b508d4bada92aa2d58
Faisal Alam <faisal.alam@rentomojo.com>
2020-03-28 23:47:09 +0530

    (TypeScript) Define processEnv inteface

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_IDE_NEW_REQUEST_API: string
+  }
+
+  // interface ReactEnv extends ProcessEnv {
+  //   NODE_ENV: 'development' | 'production' | 'test'
+  //   PUBLIC_URL: string
+  // }
+  //
+  // interface Process {
+  //   env: ReactEnv;
+  // }
+}
